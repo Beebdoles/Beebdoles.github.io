497219ae9637f922c47aeed2d407f11ed1c4a059
beebdoles <beebdoles132435@gmail.com>
2026-04-29 02:22:20 -0400

    Updates

---
+++ assets/main-DTIASLnq.js
@@ -0,0 +1,4 @@
+import"./layout-BtVuy5FP.js";var e=document.querySelector(`#app`);e&&(e.innerHTML=`
+    <h1>Hello World</h1>
+    <p>I'm hosted with GitHub Pages.</p>
+  `);
---
+++ assets/test-DkHf09CN.js
@@ -0,0 +1,5 @@
+import"./layout-BtVuy5FP.js";var e=document.querySelector(`#app`);e&&(e.innerHTML=`
+    <h1>Test Page</h1>
+    <p>This is the test sub-page.</p>
+    <a href="/">← Back home</a>
+  `);
