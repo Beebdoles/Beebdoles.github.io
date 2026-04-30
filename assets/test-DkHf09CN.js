688358ba719e5ed229495e3c5104ec9ebb19a0c6
beebdoles <beebdoles132435@gmail.com>
2026-04-30 00:39:12 -0400

    Updates

--- assets/test-DkHf09CN.js
+++
@@ -1,5 +0,0 @@
-import"./layout-BtVuy5FP.js";var e=document.querySelector(`#app`);e&&(e.innerHTML=`
-    <h1>Test Page</h1>
-    <p>This is the test sub-page.</p>
-    <a href="/">← Back home</a>
-  `);
